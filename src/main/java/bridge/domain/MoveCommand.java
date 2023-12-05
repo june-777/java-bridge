421c3cb2f8cabd77a5bdf6d00957dc5f954489fc
june-777 <wlwhswnsrl96@gmail.com>
2023-12-05 15:52:05 +0900

    feat(MoveCommand): 움직임 커맨드를 나타내는 Enum

---
+++ src/main/java/bridge/domain/MoveCommand.java
@@ -0,0 +1,33 @@
+package bridge.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public enum MoveCommand {
+
+    U("U"),
+    D("D");
+
+    private static final Map<String, MoveCommand> cachedMoveCommand = new HashMap<>();
+    private final String command;
+
+    static {
+        for (MoveCommand moveCommand : values()) {
+            cachedMoveCommand.put(moveCommand.command, moveCommand);
+        }
+    }
+
+    MoveCommand(String command) {
+        this.command = command;
+    }
+
+    public static MoveCommand of(String command) {
+        return Optional.ofNullable(cachedMoveCommand.get(command))
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 이동입니다."));
+    }
+
+    public String getCommand() {
+        return command;
+    }
+}
