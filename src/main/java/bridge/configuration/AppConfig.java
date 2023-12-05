2a25df5c1ced32f1c8fcabb649c6cbaee5a5cdaa
june-777 <wlwhswnsrl96@gmail.com>
2023-12-05 17:57:32 +0900

    feat(AppConfig): DI 로직 추가

--- src/main/java/bridge/configuration/AppConfig.java
+++ src/main/java/bridge/configuration/AppConfig.java
@@ -4,13 +4,18 @@ import bridge.BridgeMaker;
 import bridge.BridgeNumberGenerator;
 import bridge.BridgeRandomNumberGenerator;
 import bridge.controller.BridgeController;
+import bridge.controller.FrontController;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class AppConfig {
 
-    public BridgeController bridgeController() {
-        return new BridgeController(inputView(), outputView(), bridgeMaker());
+    public FrontController frontController() {
+        return new FrontController(inputView(), outputView(), bridgeController(), bridgeMaker());
+    }
+
+    private BridgeController bridgeController() {
+        return new BridgeController(inputView(), outputView());
     }
 
     private InputView inputView() {
