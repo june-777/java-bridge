1ba1c314de4bb9949caba72683641c2126651092
june-777 <wlwhswnsrl96@gmail.com>
2023-12-05 13:52:30 +0900

    feat(BridgeTile): 다리 한칸의 타일을 나타내는 Enum

---
+++ src/main/java/bridge/domain/BridgeTile.java
@@ -0,0 +1,31 @@
+package bridge.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public enum BridgeTile {
+    U(1),
+    D(0);
+
+    private static final Map<Integer, BridgeTile> cachedTile = new HashMap<>();
+
+    static {
+        for (BridgeTile tile : values()) {
+            cachedTile.put(tile.tileNumber, tile);
+        }
+    }
+
+    private final int tileNumber;
+
+    BridgeTile(int tileNumber) {
+        this.tileNumber = tileNumber;
+    }
+
+    public static String of(int tileNumber) {
+        BridgeTile bridgeTile = Optional.ofNullable(cachedTile.get(tileNumber))
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 타일 번호입니다."));
+
+        return bridgeTile.name();
+    }
+}
