170a66909c957e565865c5181120d000a7ece0c3
june-777 <wlwhswnsrl96@gmail.com>
2023-12-05 18:45:01 +0900

    test(BridgeGameTest): 다리 게임 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -1,38 +1,45 @@
 package bridge;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import bridge.domain.bridge.BridgeGame;
+import bridge.domain.bridge.BridgeGameStatus;
+import bridge.domain.moving.MoveCommand;
+import bridge.domain.moving.MoveResult;
+import bridge.domain.moving.MovingStatus;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class BridgeGameTest {
 
     @Test
-    void temp() {
-        // given
-//        List<String> bridge = List.of("U", "U", "D");
-//        BridgeGame bridgeGame = new BridgeGame(bridge);
-//
-//        BridgeGameStatus status1 = bridgeGame.move(MoveCommand.U);
-//        BridgeGameStatus status2 = bridgeGame.move(MoveCommand.D);
-//
-//        assertThat(status1).isEqualTo(BridgeGameStatus.SUCCESS);
-//        assertThat(status2).isEqualTo(BridgeGameStatus.FAIL);
-//
-//        BridgeGameStatus status3 = bridgeGame.move(MoveCommand.U);
-//        BridgeGameStatus status4 = bridgeGame.move(MoveCommand.U);
-//        BridgeGameStatus status5 = bridgeGame.move(MoveCommand.U);
-//
-//        assertThat(status3).isEqualTo(BridgeGameStatus.SUCCESS);
-//        assertThat(status4).isEqualTo(BridgeGameStatus.SUCCESS);
-//        assertThat(status5).isEqualTo(BridgeGameStatus.FAIL);
-//
-//        BridgeGameStatus status6 = bridgeGame.move(MoveCommand.U);
-//        BridgeGameStatus status7 = bridgeGame.move(MoveCommand.U);
-//        BridgeGameStatus status8 = bridgeGame.move(MoveCommand.D);
-//
-//        assertThat(status6).isEqualTo(BridgeGameStatus.SUCCESS);
-//        assertThat(status7).isEqualTo(BridgeGameStatus.SUCCESS);
-//        assertThat(status8).isEqualTo(BridgeGameStatus.SUCCESS);
-//
-//        BridgeGameStatus status10 = bridgeGame.move(MoveCommand.D);
-//        assertThat(status10).isEqualTo(BridgeGameStatus.FINISH);
+    void test() {
+        List<String> bridge = List.of("U", "U", "D");
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+
+        MoveResult status1 = bridgeGame.move(MoveCommand.U);
+        MoveResult status2 = bridgeGame.move(MoveCommand.D);
+
+        assertThat(status1.movingStatus()).isEqualTo(MovingStatus.SUCCESS);
+        assertThat(status2.movingStatus()).isEqualTo(MovingStatus.FAIL);
+
+        MoveResult status3 = bridgeGame.move(MoveCommand.U);
+        MoveResult status4 = bridgeGame.move(MoveCommand.U);
+        MoveResult status5 = bridgeGame.move(MoveCommand.U);
+
+        assertThat(status3.movingStatus()).isEqualTo(MovingStatus.SUCCESS);
+        assertThat(status4.movingStatus()).isEqualTo(MovingStatus.SUCCESS);
+        assertThat(status5.movingStatus()).isEqualTo(MovingStatus.FAIL);
+
+        MoveResult status6 = bridgeGame.move(MoveCommand.U);
+        MoveResult status7 = bridgeGame.move(MoveCommand.U);
+        MoveResult status8 = bridgeGame.move(MoveCommand.D);
+
+        assertThat(status6.movingStatus()).isEqualTo(MovingStatus.SUCCESS);
+        assertThat(status7.movingStatus()).isEqualTo(MovingStatus.SUCCESS);
+        assertThat(status8.movingStatus()).isEqualTo(MovingStatus.SUCCESS);
+
+        BridgeGameStatus finalStatus = bridgeGame.getStatus();
+        assertThat(finalStatus).isEqualTo(BridgeGameStatus.FINISH);
     }
 }
