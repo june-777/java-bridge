6a1dcd96a67dab2d496ee6fb9a88f441915335fa
june-777 <wlwhswnsrl96@gmail.com>
2023-12-05 15:53:29 +0900

    feat(OutputView): 이동한 칸에 대한 결과를 출력하는 기능

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,9 @@
 package bridge.view;
 
+import bridge.domain.BridgeGameStatus;
+import java.util.List;
+import java.util.StringJoiner;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -24,7 +28,45 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    // TODO: 메서드 분리, 상수처리 필요
+    public void printMap(List<String> bridge, int currentPosition, BridgeGameStatus result) {
+        StringJoiner upBridgeFormat = new StringJoiner(" | ", "[ ", " ]");
+        StringJoiner downBridgeFormat = new StringJoiner(" | ", "[ ", " ]");
+        for (int position = 0; position < currentPosition; position++) {
+            String tile = bridge.get(position);
+            if (tile.equals("U")) {
+                upBridgeFormat.add("O");
+                downBridgeFormat.add(" ");
+                continue;
+            }
+            upBridgeFormat.add(" ");
+            downBridgeFormat.add("O");
+        }
+        String lastTile = bridge.get(currentPosition);
+        if (lastTile.equals("U")) {
+            if (result == BridgeGameStatus.SUCCESS) {
+                upBridgeFormat.add("O");
+                downBridgeFormat.add(" ");
+            } else {
+                upBridgeFormat.add(" ");
+                downBridgeFormat.add("X");
+            }
+        }
+        if (lastTile.equals("D")) {
+            if (result == BridgeGameStatus.FAIL) {
+                upBridgeFormat.add("X");
+                downBridgeFormat.add(" ");
+            } else {
+                upBridgeFormat.add(" ");
+                downBridgeFormat.add("O");
+            }
+        }
+        printBridgeResult(upBridgeFormat, downBridgeFormat);
+    }
+
+    private static void printBridgeResult(StringJoiner upBridgeFormat, StringJoiner downBridgeFormat) {
+        System.out.println(upBridgeFormat);
+        System.out.println(downBridgeFormat);
     }
 
     /**
